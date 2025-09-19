b00ff2b5b02290873ab435cc7943f24a85b930d4
Chornoknysh <danuk123boss@gmail.com>
2025-09-19 20:22:52 +0200

    solution

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
+kubectl apply -f .infrastructure/namespace.yml
+kubectl wait --for=condition=Bound pvc/todo-pvc -n todoapp --timeout=60s
 
 echo "Applying PersistentVolume..."
-kubectl apply -f pv.yml
+kubectl apply -f .infrastructure/pv.yml
 
 echo "Applying PersistentVolumeClaim..."
-kubectl apply -f pvc.yml
+kubectl apply -f .infrastructure/pvc.yml
 
 echo "Applying Deployment..."
-kubectl apply -f deployment.yml
+kubectl apply -f .infrastructure/deployment.yml
 
 echo "All resources applied."
 echo "Run 'kubectl get pv,pvc,pods -o wide' to check status."
